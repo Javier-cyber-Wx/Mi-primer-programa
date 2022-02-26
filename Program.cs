8180b8844b439de6412c3b7d3573c55295aaa7b2
Javier Chamo <francisco12102002cl@gmail.com>
2022-02-25 20:34:24 -0600

    25 de febrero

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tarea_programacion_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hazlo facil, haz Click!");
+        }
+    }
+}
